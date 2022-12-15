54b94564f68325f43bf0f0634027afe122256b36
Pandora2412 <101192378+Pandora2412@users.noreply.github.com>
2022-12-15 17:52:23 +0700

    Add files via upload

---
+++ hotel_UI/src/pages/Admin/quanly_app.js
@@ -0,0 +1,78 @@
+import React, { Component } from "react";
+import '../../css/quanly_style.css';
+import Header from "../../components/Admin/Header";
+import img from '../../Image/img.png';
+import create from '../../Image/create.png';
+import { Link } from "react-router-dom";
+
+console.log(img);
+console.log(create);
+
+class Quanly_app extends Component {
+    render() {
+        return (
+            <div className="myForm">
+                <Header></Header>
+                <div className="empty"></div>
+                <div className="r">
+                    <div className="Col">
+                        <div className="Info">Thông tin cá nhân</div>
+                        <form>
+                            <div className="empty"></div>
+                            <div className="part1">
+                                <label>Họ tên:
+                                    <br></br>
+                                    <input type="text" />
+                                </label>
+                                <div className="empty"></div>
+                                <label>SĐT:
+                                    <br></br>
+                                    <input type="text" />
+                                </label>
+                            </div>
+                            <div className="part2">
+                                <label>CCCD:
+                                    <br></br>
+                                    <input type="text" />
+                                </label>
+                                <div className="empty"></div>
+                                <label>Email:
+                                    <br></br>
+                                    <input type="text" />
+                                </label>
+                            </div>
+                        </form>
+                        <div className="empty"></div>
+                        <div className="Info">Thông tin tài khoản</div>
+                        <form>
+                            <div className="empty"></div>
+                            <div className="part1">
+                                <label>Tên đăng nhập:
+                                    <br></br>
+                                    <input type="text" />
+                                </label>
+                            </div>
+                            <div className="part2">
+                                <label>Mật khẩu:
+                                    <br></br>
+                                    <input type="text" />
+                                </label>
+                            </div>
+                        </form>
+                    </div>
+                    <div className="img">
+                        <img src={img} alt="" />
+                    </div>
+                </div>
+                <div className="empty"></div>
+                <div className="button">
+                    <Link to="/admin/after_themquanly">
+                        <img src={create} alt="" />
+                    </Link>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default Quanly_app;
