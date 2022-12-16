259c9b02e40ac42123b60570a3ce78e38561e201
Pandora2412 <101192378+Pandora2412@users.noreply.github.com>
2022-12-16 10:28:48 +0700

    Update Admin.js

--- hotel_UI/src/routes/Admin.js
+++ hotel_UI/src/routes/Admin.js
@@ -2,9 +2,9 @@ import { Outlet } from 'react-router-dom';
 
 const Admin = () => {
     return (
-        <>
+        <div className="Admin">
             <Outlet/>
-        </>
+        </div>
     );
 }
 
